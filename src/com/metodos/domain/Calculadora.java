c79b056084066c629ec6451a30d1bef185bc3691
aerolitus <christianfernandes018@gmail.com>
2025-03-19 15:05:14 -0300

    testes com metodo void

---
+++ src/com/metodos/domain/Calculadora.java
@@ -0,0 +1,10 @@
+package com.metodos.domain;
+
+public class Calculadora {
+    public void soma (){
+        System.out.println(10+10);
+    }
+    public void subtracao(){
+        System.out.println(10-10);
+    }
+}
---
+++ src/com/metodos/domain/test/CalculadoraTeste01.java
@@ -0,0 +1,11 @@
+package com.metodos.domain.test;
+
+import com.metodos.domain.Calculadora;
+
+public class CalculadoraTeste01 {
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        calc.soma();
+        calc.subtracao();
+    }
+}
